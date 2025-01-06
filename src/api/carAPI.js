a30b76545bdb51fc2c7f01ec26d49c0d8235e45e
KxllSwxtch <dimashin9@gmail.com>
2025-01-06 19:12:36 +0900

    udpates

--- src/api/carAPI.js
+++ src/api/carAPI.js
@@ -1,4 +1,5 @@
-const BASE_URL = 'http://127.0.0.1:8000/api/proxy/filter/page'
+// const BASE_URL = 'http://127.0.0.1:8000/api/proxy/filter/page'
+const BASE_URL = `${import.meta.env.VITE_API_URL}/api/proxy/filter/page`
 
 /**
  * Функция для получения автомобилей с учетом фильтров и страницы.

4cfce10902fa2e6159e1db97a52e8cdc2ad85123
KxllSwxtch <dimashin9@gmail.com>
2025-01-07 18:24:59 +0900

    finished

---
+++ src/data/generations_backup.js
@@ -0,0 +1,63 @@
+const generations = {
+	// Aston Martin
+	2586: [{ id: 303, name: 'I' }],
+	45: [{ id: 320, name: 'Bulldog' }],
+	1800: [{ id: 302, name: 'Cygnet' }],
+	2509: [{ id: 329, name: 'DB5' }],
+	51: [
+		{ id: 325, name: 'I' },
+		{ id: 5787, name: 'IV' },
+		{ id: 327, name: 'III' },
+		{ id: 326, name: 'II' },
+	],
+	2058: [{ id: 301, name: 'One-77' }],
+	2813: [{ id: 5568, name: 'DB AR1' }],
+	49: [{ id: 324, name: 'Tickford Capri' }],
+	1531: [
+		{ id: 314, name: 'I' },
+		{ id: 315, name: 'I (S)' },
+	],
+	47: [
+		{ id: 305, name: 'I' },
+		{ id: 307, name: 'I 2' },
+	],
+	1482: [
+		{ id: 5956, name: 'III' },
+		{ id: 310, name: 'I' },
+		{ id: 311, name: 'II' },
+	],
+	53: [{ id: 323, name: 'V8 Zagato' }],
+	52: [
+		{ id: 322, name: 'II' },
+		{ id: 321, name: 'I' },
+	],
+	1987: [{ id: 319, name: 'V12 Zagato' }],
+	50: [
+		{ id: 318, name: 'II' },
+		{ id: 317, name: 'I' },
+	],
+	48: [{ id: 309, name: 'Lagonda' }],
+	3102: [{ id: 6351, name: 'I' }],
+	46: [{ id: 312, name: 'I' }],
+	3323: [{ id: 7179, name: 'Vulcan' }],
+
+	// Audi
+	1461: [
+		{ id: 5121, name: 'II (F5)' },
+		{ id: 185, name: 'I (8T)' },
+	],
+	72: [
+		{ id: 287, name: 'I (C4)' },
+		{ id: 6280, name: 'V (C8)' },
+		{ id: 283, name: 'II (C5)' },
+		{ id: 285, name: 'IV (C7)' },
+		{ id: 284, name: 'III (C6)' },
+	],
+	2583: [{ id: 228, name: 'I' }],
+	3096: [
+		{ id: 6353, name: 'I' },
+		{ id: 6535, name: 'R8 LMP' },
+	],
+}
+
+export default generations

e7c4c8df0dff587b0d6e889b846724f6a794a207
KxllSwxtch <dimashin9@gmail.com>
2025-01-21 11:11:11 +0900

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -17,37 +17,28 @@ import {
 } from './pages'
 
 const App = () => {
-	const location = useLocation()
-
-	const canonicalUrl = `https://www.glory-traders.org${location.pathname}`
-
 	return (
-		<>
-			<Helmet>
-				<link rel='canonical' href={canonicalUrl} />
-			</Helmet>
-			<Router>
-				<ScrollToTop />
+		<Router>
+			<ScrollToTop />
 
-				<div className='flex flex-col min-h-screen'>
-					<Header />
-					<main className='flex-grow pt-[64px] md:pt-[80px]'>
-						<div className='mx-auto'>
-							<Routes>
-								<Route path='/' element={<Home />} />
-								<Route path='/catalog' element={<Catalog />} />
-								<Route path='/contacts' element={<Contacts />} />
-								<Route path='/cars/:id' element={<CarDetails />} />
-								<Route path='/privacy-policy' element={<PrivacyPolicy />} />
-								<Route path='/calculator' element={<Calculator />} />
-								<Route path='/faq' element={<FAQ />} />
-							</Routes>
-						</div>
-					</main>
-					<Footer />
-				</div>
-			</Router>
-		</>
+			<div className='flex flex-col min-h-screen'>
+				<Header />
+				<main className='flex-grow pt-[64px] md:pt-[80px]'>
+					<div className='mx-auto'>
+						<Routes>
+							<Route path='/' element={<Home />} />
+							<Route path='/catalog' element={<Catalog />} />
+							<Route path='/contacts' element={<Contacts />} />
+							<Route path='/cars/:id' element={<CarDetails />} />
+							<Route path='/privacy-policy' element={<PrivacyPolicy />} />
+							<Route path='/calculator' element={<Calculator />} />
+							<Route path='/faq' element={<FAQ />} />
+						</Routes>
+					</div>
+				</main>
+				<Footer />
+			</div>
+		</Router>
 	)
 }
 
